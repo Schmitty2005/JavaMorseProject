1f4feb7ec68f8b593ed3f4aabf86cf40a9a07de8
Bill Smith Linux <schmitty2005@gmail.com>
2014-11-23 08:06:28 -0800

    Fixed wave Header creation code to work properly.

--- src/WavePackage/WaveTools.java
+++ src/WavePackage/WaveTools.java
@@ -89,13 +89,23 @@ public class WaveTools {
         //TODO this is for input into the createWaveHeaderForPcm sub!
         //TODO find out how to add a byte array to this !
     }
-public static void putUnsignedInt(ByteBuffer bb, long value) {
-    long test = value & 0x0000ffffL;
-    int tester = (int)test;
-    System.out.println(String.format("%02X", test));
-    System.out.println(String.format("%02X", tester));//String. poostring = (byte[])tester;
-    bb.putInt((int) (value & 0x0001ffffL));
+    
+    /**
+ * Used for writing Unsigned Integer to Byte Array.
+ * @param out
+ * @param val 
+ */
+    private static void writeInt(ByteBuffer buffer, int intToUnsign) {
+//TODO add suppress warning here
+        buffer.put((byte) (intToUnsign >> 0));
+        buffer.put((byte) (intToUnsign >> 8));
+        buffer.put((byte) (intToUnsign >> 16));
+        buffer.put((byte) (intToUnsign >> 24));
+        //out.write(val >> 8);
+        //out.write(val >> 16);
+        //out.write(val >> 24);
     }
+
     /**
      * This method accepts a byte array and creates and attaches a wave header
      * to the data.
@@ -153,8 +163,10 @@ public static void putUnsignedInt(ByteBuffer bb, long value) {
         waveBuffer.putShort(numberChannels); //2   NumChannels      Mono = 1, Stereo = 2, etc.
         //WARNING !!  JAVA IS TRASH! I had to manually input these byte because there is no such thing
         // as unsigned integers!  What kind of pile of shit is this?
-        byte[] comboRate = {0x44, (byte)0xac, (byte)0x00, (byte)0x00, (byte)0x88, 0x58, 0x01, (byte)0x00};
-        waveBuffer.put((byte[])comboRate);
+            //byte[] comboRate = {0x44, (byte)0xac, (byte)0x00, (byte)0x00, (byte)0x88, 0x58, 0x01, (byte)0x00};
+            //waveBuffer.put((byte[])comboRate);
+        writeInt(waveBuffer, sampleRate);
+        writeInt(waveBuffer, byteRate);
                     //putUnsignedInt(waveBuffer, (long)sampleRate);
                     //putUnsignedInt(waveBuffer, (long)byteRate);
                     //waveBuffer.putInt((int)((long)sampleRate & 0xffffffffL));  //sample rate This needs to be an Unsigned INT also!
@@ -209,7 +221,7 @@ public static void putUnsignedInt(ByteBuffer bb, long value) {
             sliceValue = bb.getShort(s * 2);
             bb.putShort((s * 2), (short) (weight * sliceValue));     // Fade In
             bb.putShort((s * 2), (short) (weight * sliceValue));                       // Fade In
-            
+            //TODO fix this so there is no tail on end of wave! 
             sliceValue = bb.getShort((pcmData.length - 4 - (2 * s)));
             bb.putShort((pcmData.length - (s * 2) - 4), (short) (sliceValue * weight));  // Fade Out
         }
@@ -331,7 +343,7 @@ int numberSlices = (int)calcSlices;
 
 //Add wave Header to PCM
         byte[] testWaveWithHeader;
-        testWaveWithHeader = createWaveHeaderForPcm(testPCMsine, (short) 44100, (short) 16);
+        testWaveWithHeader = createWaveHeaderForPcm(testPCMsine,  44100, (short) 16);
         System.out.println(testWaveWithHeader.length);
         saveToWaveFile(testPCMsine, "testpcm.pcm");  // pcm data looks beautiful in audacity!
         saveToWaveFile(testWaveWithHeader, "testwave.wav"); // wave wont load :( in Audacity
@@ -357,7 +369,7 @@ int numberSlices = (int)calcSlices;
         
         saveToWaveFile(workingByte, "combined.pcm");
         byte [] waveToSave;
-        waveToSave = createWaveHeaderForPcm(workingByte, (short)sampleRate, (short)16);
+        waveToSave = createWaveHeaderForPcm(workingByte, sampleRate, (short)16);
         saveToWaveFile(waveToSave,"combined.wav");
         
         
@@ -374,80 +386,3 @@ int numberSlices = (int)calcSlices;
     }
 
 }
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-//////END OF JAVA CODE!//////////////////////END OF JAVA CODE!//////////////////
-/*WORKING CODE FROM MY VB.NET PROJECT
- Function createWave(ByRef genStream As MemoryStream, ByVal frequency As Double, ByVal msDuration As Integer, _
- Optional ByVal msTrailingSilence As Integer = 0, Optional msRamp As Integer = 4, Optional ByVal volume As UInt16 = 16383) ' 16383
- ' createWave generates a sine wave in the form of a memory stream to be passed to windows.media.player
- ' frequency is frequency in Hertz, msDuration is tone duration in milliseconds, msRamp is the beginning and ending
- ' volume ramp (5ms is standard CW)
- 'set variables
- Dim writer As New BinaryWriter(genStream)
- Dim TAU As Double = 2 * Math.PI
- Dim formatChunkSize As Integer = 16
- Dim headerSize As Integer = 8
- Dim formatType As Short = 1
- Dim tracks As Short = 1
- Dim samplesPerSecond As Integer = 44100
- Dim bitsPerSample As Short = 16
- Dim frameSize As Short = 2 ' manual input of 2 because it should be right! CShort(tracks * ((bitsPerSample + 7) \ 8))
- Dim bytesPerSecond As Integer = samplesPerSecond * frameSize
- Dim waveSize As Integer = 2 ' change from 4 to 2
- Dim total_samples As Integer = CInt(Math.Truncate(CType(samplesPerSecond, [Decimal]) * CDbl(msDuration * 0.001))) 'removed /1000 from both
- Dim rampSamples As Integer = CInt(Math.Truncate(CType(samplesPerSecond, [Decimal]) * msRamp * 0.001)) 'number of samples for ramp
- Dim full_amplitude_samples As Integer = total_samples - (rampSamples * 2) 'number of samples at full amplitude
- Dim silenceSamples As Integer = CInt(Math.Truncate(CType(samplesPerSecond, [Decimal]) * CDbl(msTrailingSilence * 0.001)))
- Dim dataChunkSize As Integer = (total_samples * 2) + (silenceSamples * 2) 'frameSize --- changed 1 to 2 5/28/14
- Dim fileSize As Integer = 36 + dataChunkSize 'waveSize + headerSize + formatChunkSize + headerSize + dataChunkSize
- ' var encoding = new System.Text.UTF8Encoding();
- writer.Write(&H46464952) ' = encoding.GetBytes("RIFF")
- writer.Write(fileSize)
- writer.Write(&H45564157) ' = encoding.GetBytes("WAVE")
- writer.Write(&H20746D66) ' = encoding.GetBytes("fmt ")
- writer.Write(formatChunkSize) ' = 16 for 'PCM'
- writer.Write(formatType) ' 1 for 'PCM'
- writer.Write(tracks) ' number of channels
- writer.Write(samplesPerSecond) ' sample rate
- writer.Write(bytesPerSecond) ' ByteRate == SampleRate * NumChannels * BitsPerSample/8
- writer.Write(frameSize) ' AKA 'Block Align' according to https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
- writer.Write(bitsPerSample) ' 8 bit or 16 bit sound sample etc....
- writer.Write(&H61746164) ' = encoding.GetBytes("data")
- writer.Write(dataChunkSize) ' == NumSamples * NumChannels * BitsPerSample/8
- ' removed -1 from samples per second. CDbl(samplesPerSecond -1)
- ' 'volume' is UInt16 with range 0 thru Uint16.MaxValue ( = 65 535)
- ' we need 'amp' to have the range of 0 thru Int16.MaxValue ( = 32 767)
- Dim amp As Double = volume >> 2 ' so we simply set amp = volume / 2
- Dim rampAmp As Double = 0
- 'create amplification ramp of wave for number of ramp samples (duration of msRamp)
- For [step] As Integer = 0 To rampSamples
- rampAmp = CDbl([step]) / CDbl(rampSamples)
- Dim s As Short = CShort(((amp) * Math.Sin(theta * CDbl([step]))))
- s = s * rampAmp
- writer.Write(s)
- Next [step]
- 'amp = volume >> 2 uncommented on 5/30/14 should be OKAY
- ' create regular amplitude wave for full duration minus beginning and ending ramp
- For [step] As Integer = (rampSamples + 1) To (full_amplitude_samples + rampSamples)
- Dim s As Short = CDbl((amp * Math.Sin(theta * CDbl([step]))))
- writer.Write(s)
- Next [step]
- 'create ending ramp amplfication from full volume to 0
- For [step] As Integer = (rampSamples + full_amplitude_samples + 1) To total_samples + 1
- rampAmp = (total_samples + 1 - [step]) / CDbl(rampSamples)
- Dim s As Short = CShort(((amp) * Math.Sin(theta * CDbl([step]))))
- s = CShort(s * rampAmp)
- writer.Write(s)
- Next [step]
- 'routine to add trailing silence to waves for cleaner sound. Ex. Incorporate interspace at end of dit or dah
- ' instead of separate interspace wave....interspace wave may be causing clicks and pops in audio playback.
- If msTrailingSilence <> 0 Then
- For [step] As Integer = 0 To silenceSamples
- writer.Write(0)
- Next
- End If
- If genStream.Length Mod 2 <> 0 Then writer.Write(0) ' add extra sample to keep samples even
- Return genStream
- End Function
- */
