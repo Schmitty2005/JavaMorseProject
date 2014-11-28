13f9670e698159c690acc5900c714d5118adc826
Bill Smith Linux <schmitty2005@gmail.com>
2014-11-27 19:37:24 -0800

    Added untested routine to mix PCM byte arrays.

---
+++ src/morseplayer/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Used in the playback of a string or single character in morse code
+ * at selected frequency and words per minute.
+ * 
+ * Farnsworth spacing option is also available
+ */
+package morseplayer;
