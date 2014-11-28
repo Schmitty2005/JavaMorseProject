ab626f76c8cf5cbac288407e152326f6285b0b13
Bill Smith Linux <schmitty2005@gmail.com>
2014-11-27 19:37:24 -0800

    Added untested routine to mix PCM byte arrays.
    Conflicts:
    	src/morseplayer/package-info.java

--- src/morseplayer/package-info.java
+++ src/morseplayer/package-info.java
@@ -4,12 +4,18 @@
  * and open the template in the editor.
  */
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> 8341a0c... Added untested routine to mix PCM byte arrays.
 /**
  * Used in the playback of a string or single character in morse code
  * at selected frequency and words per minute.
  * 
  * Farnsworth spacing option is also available
  */
+<<<<<<< HEAD
+>>>>>>> 8341a0c... Added untested routine to mix PCM byte arrays.
+=======
 >>>>>>> 8341a0c... Added untested routine to mix PCM byte arrays.
 package morseplayer;
