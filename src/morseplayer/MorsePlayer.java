210ab2a975ee47963e5d416315cecc2f08863e9b
Bill Smith <schmitty2008@gmail.com>
2014-11-30 09:01:15 -0800

    Hopefully merged properly

--- src/morseplayer/MorsePlayer.java
+++ src/morseplayer/MorsePlayer.java
@@ -3,9 +3,9 @@
  */
 package morseplayer;
 
-
 /**
- *This is a description.
+ * This is a description.
+ *
  * @author bill
  */
 import javax.sound.sampled.*;
@@ -13,112 +13,105 @@ import java.io.*;
 import jdk.nashorn.internal.codegen.CompilerConstants;
 //import sun.font.TrueTypeFont; //WTF??? How did this get in?
 
-/**Class Descripition added right before class
+/**
+ * Class Descripition added right before class
  */
 class MorsePlayer {
-/**MorsePlayer Class is used to playback morse code.  
- * Strings and Characters are played back at a speed determined in words per minute
- * 
- * @param frequency is the frequency of the tone that is desired
- * @param wordsPerMinute is the speed in words per minute.  Default is 15 WPM
- * @param farnsworthSpacing determines is farnsworth spacing will be used.
- * @param charToPlay is used to play a single character in morse code.
- * @param stringToPlay is used to play a word or sentence in morse code.
- * 
-* 
-*/
-            
-Sound_Timing soundDurations = new Sound_Timing(12, 6, false);
-MorseDictionary DecodeDictionary = new MorseDictionary();  
-    
-private int frequency = 800;
-private int wordsPerMinute = 15;
-private boolean farnsworthSpacing = false;
-private char charToPlay;
-private String wordToPlay;
 
-        MorsePlayer() {
-            //Default Constructor
-        }
-	
-	MorsePlayer (int WPM){
-		this.wordsPerMinute = WPM;
-	}
-	
-	MorsePlayer (int WPM, boolean use_farnsworth, int freq_hz){
-		this.wordsPerMinute = WPM;
-		this.farnsworthSpacing = use_farnsworth;
-		this.frequency = freq_hz;
-	}
-        
-	MorsePlayer (int WPM, double freq_hz){
-		this.frequency = (int)freq_hz;
-                this.wordsPerMinute = WPM;
-	}
-        
-	MorsePlayer (boolean use_farnsworth){
-		this.farnsworthSpacing = use_farnsworth;
-	}	
-	
-        private static void playDit ()
-        {
-            System.out.println("Beep     - Dit beep");
-            //TODO code to play dit sound
-	}
-	private static void playDah ()
-        {
-            System.out.println("Beeeeeep - Dah beep");
-            //TODO code to play dah sound
-	}
-	public void playChar (char characterToPlay){
-            //TODO for some reason, Dictionary was not working properly....may need '' instead of ""
-            String morseString = DecodeDictionary.morseDictionary.get (characterToPlay);
-            //String morseString = "...-.-";
-            int index = 0;
-            int stringLength = morseString.length();
-            while ( index < stringLength)
-            {
-                char morseElement = morseString.charAt(index);
-                    if (morseElement == '.')
-                        {
-                         playDit();
-                        }
-                    else if(morseElement == '-') 
-                        {
-                        playDah();
-                        }
+    /**
+     * MorsePlayer Class is used to playback morse code. Strings and Characters
+     * are played back at a speed determined in words per minute
+     *
+     * @param frequency is the frequency of the tone that is desired
+     * @param wordsPerMinute is the speed in words per minute. Default is 15 WPM
+     * @param farnsworthSpacing determines is farnsworth spacing will be used.
+     * @param charToPlay is used to play a single character in morse code.
+     * @param stringToPlay is used to play a word or sentence in morse code.
+     *     
+*
+     */
+
+    Sound_Timing soundDurations = new Sound_Timing(12, 6, false);
+    MorseDictionary DecodeDictionary = new MorseDictionary();
+
+    private int frequency = 800;
+    private int wordsPerMinute = 15;
+    private boolean farnsworthSpacing = false;
+    private char charToPlay;
+    private String wordToPlay;
+
+    MorsePlayer() {
+        //Default Constructor
+    }
+
+    MorsePlayer(int WPM) {
+        this.wordsPerMinute = WPM;
+    }
+
+    MorsePlayer(int WPM, boolean use_farnsworth, int freq_hz) {
+        this.wordsPerMinute = WPM;
+        this.farnsworthSpacing = use_farnsworth;
+        this.frequency = freq_hz;
+    }
+
+    MorsePlayer(int WPM, double freq_hz) {
+        this.frequency = (int) freq_hz;
+        this.wordsPerMinute = WPM;
+    }
+
+    MorsePlayer(boolean use_farnsworth) {
+        this.farnsworthSpacing = use_farnsworth;
+    }
+
+    private static void playDit() {
+        System.out.println("Beep     - Dit beep");
+        //TODO code to play dit sound
+    }
+
+    private static void playDah() {
+        System.out.println("Beeeeeep - Dah beep");
+        //TODO code to play dah sound
+    }
+
+    public void playChar(char characterToPlay) {
+        //TODO for some reason, Dictionary was not working properly....may need '' instead of ""
+        String morseString = DecodeDictionary.morseDictionary.get(characterToPlay);
+        //String morseString = "...-.-";
+        int index = 0;
+        int stringLength = morseString.length();
+        while (index < stringLength) {
+            char morseElement = morseString.charAt(index);
+            if (morseElement == '.') {
+                playDit();
+            } else if (morseElement == '-') {
+                playDah();
+            }
             index++;
-            }  //TODO code to play morse character --finished 11/10/2014
-	}
-        
-	public void playWord (String wordToPlay)
-        {
-            int stringLength = wordToPlay.length();
-            String wordToPlayLowerCase = wordToPlay.toLowerCase();
-            int index = 0;
-            
-            while (index < stringLength)
-            {
-                char charToPlay = wordToPlayLowerCase.charAt(index);
-                playChar(charToPlay);
-                System.out.println("===================");
-                index ++;
+        }  //TODO code to play morse character --finished 11/10/2014
+    }
+
+    public void playWord(String wordToPlay) {
+        int stringLength = wordToPlay.length();
+        String wordToPlayLowerCase = wordToPlay.toLowerCase();
+        int index = 0;
+
+        while (index < stringLength) {
+            char charToPlay = wordToPlayLowerCase.charAt(index);
+            playChar(charToPlay);
+            System.out.println("===================");
+            index++;
             //TODO remember to add code that accounts for spacing between words!
-            }//TODO code to play string in morse	
-	}
-        
-      public static void main (String [] args){
+        }//TODO code to play string in morse	
+    }
+
+    public static void main(String[] args) {
 
     //String poop = DecodeDictionary.morseDictionary.get ("a");
-    
-
-
 //These are testing lines
 /*
           
-          */
+         */
 //Testing lines end here.....
 //TODO main code....initialize sounds (create waveforms)
-	}  
+    }
 }
-	
---
+++ src/morseplayer/PlayMorse.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package morseplayer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+//import java.nio.ByteBuffer;
+//import morseplayer.MorseDictionary;
+//import morseplayer.MorseElements;
+
+/**
+ *
+ * @author bill
+ */
+public class PlayMorse {
+
+    int mWPM;
+    //Sound_Timing timing = new Sound_Timing(18);
+    private MorseElements elements = new MorseElements(32, 800, false);
+    private MorseDictionary morseDict = new MorseDictionary();
+
+    public PlayMorse(int mWPM) {
+        Sound_Timing timing = new Sound_Timing(mWPM);
+        this.elements = new MorseElements(mWPM, 800, false);
+    }
+
+    ;
+  
+    PlayMorse(int mWPM, int freq_hz) {
+        Sound_Timing timing = new Sound_Timing(mWPM);
+        this.elements = new MorseElements(mWPM, freq_hz, false);
+    }
+
+    ;
+  
+    PlayMorse(int mWPM, int freq_hz, boolean farnsworthSpacing) {
+        Sound_Timing timing = new Sound_Timing(mWPM);
+        this.elements = new MorseElements(mWPM, 800, farnsworthSpacing);
+    }
+
+    ;
+  
+    PlayMorse(int mWPM, int freq_hz, boolean farnsworthSpacing, String playString) {
+        Sound_Timing timing = new Sound_Timing(mWPM);
+        this.elements = new MorseElements(mWPM, freq_hz, farnsworthSpacing);
+        //@TODO call routine to play string in morse here!
+
+    }
+
+    ;
+  
+    PlayMorse(String playString) {
+//@TODO call routine to play string in morse here!
+    }
+
+    ;
+    PlayMorse(int mWPM, String mplayString) {
+        //Sound_Timing timing = new Sound_Timing(mWPM);
+        this.elements = new MorseElements(mWPM, 400, false);
+        playString(mplayString);
+        //@TODO call routine to play string in morse here!
+    }
+
+    ;
+public void playString(String playString) {
+//@TODO code to play string in morse :)
+        //@TODO NEEDS ByteArrayOutpuStream to work!
+        ByteArrayOutputStream bbout = new ByteArrayOutputStream();
+
+        char charToPlay;
+        int sLength = playString.length();
+        byte[] playWave = new byte[32];
+        //byte[]  constructionOne;
+        //ByteBuffer bb = ByteBuffer.wrap(playWave);
+        for (int step = 0; step < sLength; step++) {
+            charToPlay = playString.charAt(step);
+            if (charToPlay == ' ') {
+                if (elements.farnsworthSpacing) {
+//@TODO code to insert farnsworth spaced silence
+                    bbout.write(WavePackage.WaveTools.combineByteArray(playWave, elements.interCharacterFarnsworthPCM), 0, elements.interCharacterFarnsworthPCM.length);
+                } else {
+                    try {
+                        bbout.write(WavePackage.WaveTools.combineByteArray(playWave, elements.interCharacterPCM));
+                    } catch (IOException e) {
+                        System.err.println("e");
+                    }
+
+                    //playWave = constructionOne;
+                }
+            }
+            char toLower = Character.toLowerCase(charToPlay);
+            String morseString = morseDict.morseDictionary.get(toLower);
+
+            for (int x = 0; x < (morseString.length()); x++) {
+                char ditOrDah = morseString.charAt(x);
+                if (ditOrDah == '.') {
+                    try {
+
+                        bbout.write(WavePackage.WaveTools.combineByteArray(playWave, elements.ditElementPCM));
+                    } catch (IOException e) {
+                        System.err.println(e);
+                    }
+
+                    //playWave = constructionOne;
+//@TODO code to place dit in playWave
+                }
+                if (ditOrDah == '-') {
+                    try {
+                        bbout.write(WavePackage.WaveTools.combineByteArray(playWave, elements.dahElementPCM));
+                    } catch (IOException e) {
+                        System.err.println(e);
+                    }
+//playWave = constructionOne;
+//@TODO code to place dah in playWave
+                }
+
+            }
+            try {
+                bbout.write(elements.interCharacterPCM);
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+
+//@TODO code to combine dit, dah, and spacing PCM's into new byte array
+        }
+
+        playWave = bbout.toByteArray();
+        byte[] header = WavePackage.WaveTools.createWaveHeaderForPcm(playWave, 44100, (short) 16);
+        //byte [] saveWave = WavePackage.WaveTools.combineByteArray(header, playWave);
+        WavePackage.WaveTools.saveToWaveFile(header, "firstTest.wav");
+        WavePackage.WaveTools.saveToWaveFile(elements.ditElementPCM, "ditELementPCM.pcm");
+        WavePackage.WaveTools.saveToWaveFile(elements.dahElementPCM, "dahElementPCM.pcm");
+
+    }
+
+    public static void main(String[] args) {
+        PlayMorse morstest = new PlayMorse(24, "wkrp cq cq cq de wkrp");
+        //morstest.playString("ke7gbt cq cq cq ke7gbt dx de ke7gbt cq cq cq");
+    }
+}
